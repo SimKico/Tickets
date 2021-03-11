05364c6f922fcbbb76d125bfe52d0048568a970c
weakfantasy <kovacevic.natasa.95@gmail.com>
2021-03-11 01:34:48 +0100

    Klase

---
+++ Tickets/src/beans/ManifestationType.java
@@ -0,0 +1,10 @@
+package beans;
+
+public enum ManifestationType {
+	
+	CONCERTS,
+	FESTIVALS,
+	THEATERS,
+	SPORTS
+
+}
---
+++ Tickets/src/beans/TicketType.java
@@ -0,0 +1,9 @@
+package beans;
+
+public enum TicketType {
+	
+	VIP,
+	REGULAR,
+	FANPIT
+
+}
